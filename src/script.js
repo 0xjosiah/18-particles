b36584b0c8c1d9e3252f12cee060d6444956f976
Josiah Webb <jj.webb520@gmail.com>
2022-10-12 15:58:13 -0600

    adds random colors to particles, adds up/down animation

--- src/script.js
+++ src/script.js
@@ -27,12 +27,14 @@ const particleTexture = textureLoader.load('/textures/particles/2.png')
 // Geometry
 // const particleGeometry = new THREE.SphereGeometry(1, 32, 32)
 const particleGeometry = new THREE.BufferGeometry();
-const count = 5000;
+const count = 20000;
 
 const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 
 for (let i = 0; i < count * 3; i++) {
     positions[i] = (Math.random() - .5) * 10
+    colors[i] = Math.random()
 }
 
 particleGeometry.setAttribute(
@@ -40,16 +42,27 @@ particleGeometry.setAttribute(
     new THREE.BufferAttribute(positions, 3)
 )
 
+particleGeometry.setAttribute(
+    'color', 
+    new THREE.BufferAttribute(colors, 3)
+)
+
 // Material
 const particleMaterial = new THREE.PointsMaterial({
     size: .1,
     sizeAttenuation: true, // adds perspective to particles, if close to cam - looks big, far - looks small
-    color: 0xff88cc,
+    // color: 0xff88cc,
     alphaMap: particleTexture,
     transparent: true,
+    /**
+     * these help with depth issues with particles
+     */
     // alphaTest: .001,
     // depthTest: false, // typically only words with one color and one particle in scene, adding more colors/geometries will mess this up
     depthWrite: false,
+    blending: THREE.AdditiveBlending, // this can impact performance
+    vertexColors: true,
+
 })
 
 // Points
@@ -57,11 +70,11 @@ const particles = new THREE.Points( particleGeometry, particleMaterial )
 scene.add(particles)
 
 // Test Box
-const box = new THREE.Mesh(
-    new THREE.BoxGeometry(),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(box)
+// const box = new THREE.Mesh(
+//     new THREE.BoxGeometry(),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(box)
 
 /**
  * Sizes
@@ -115,6 +128,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update Particles
+    // particles.rotation.y = Math.sin(elapsedTime * .25)
+    // particles.rotation.x = (elapsedTime * .5)
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3
+        particleGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime)
+    }
+    particleGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
